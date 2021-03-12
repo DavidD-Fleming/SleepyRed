82e67f12eceb7defaedf02eca1220afe23f4b2ad
David Dong <DavidDong5252@gmail.com>
2021-03-12 01:32:51 -0500

    First Commit
    First version of project SleepyRed on Git. Only includes the two players with basic movement and shifting capabilities.

---
+++ Assets/Scripts/BlackPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackPlayer : MonoBehaviour
+{
+    public float speed = 7;
+
+    bool active = true;
+    float screenHalfWidthInWorldUnits;
+    float halfPlayerWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfPlayerWidth = transform.localScale.x / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // switches between active and inactive
+        if (Input.GetKeyDown("z"))
+        {
+            if (active == true)
+            {
+                active = false;
+                this.GetComponent<Renderer>().material.SetColor("_Color", new Color32(125, 25, 25, 255));
+            } else
+            {
+                active = true;
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+            }
+        }
+
+        // allows horizontal movement if player is currently active
+        if (active == true)
+        {
+            float inputX = Input.GetAxisRaw("Horizontal");
+            float velocity = inputX * speed;
+            transform.Translate(Vector2.right * velocity * Time.deltaTime);
+        }
+
+        // teleports player to the other side of the screen if it moves out of camera
+        if (transform.position.x < -screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+        }
+        if (transform.position.x > screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/WhitePlayer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhitePlayer : MonoBehaviour
+{
+    public float speed = 7;
+
+    bool active = false;
+    float screenHalfWidthInWorldUnits;
+    float screenHalfHeightInWorldUnits;
+    float halfPlayerWidth;
+    float halfPlayerHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfPlayerWidth = transform.localScale.x / 2f;
+        halfPlayerHeight = transform.localScale.y / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize - halfPlayerWidth;
+        screenHalfHeightInWorldUnits = Camera.main.orthographicSize - halfPlayerHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // switches between active and inactive
+        if (Input.GetKeyDown("z"))
+        {
+            if (active == true)
+            {
+                active = false;
+                // changes the player to a "sleepy" blue
+                this.GetComponent<Renderer>().material.SetColor("_Color", new Color32(50, 50, 125, 255));
+            }
+            else
+            {
+                active = true;
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+            }
+        }
+
+        // allows horizontal and vertical movement if player is currently active
+        if (active == true)
+        {
+            float inputX = Input.GetAxisRaw("Horizontal");
+            float inputY = Input.GetAxisRaw("Vertical");
+            float velocityX = inputX * speed;
+            float velocityY = inputY * speed;
+            transform.Translate(Vector2.right * velocityX * Time.deltaTime);
+            transform.Translate(Vector2.up * velocityY * Time.deltaTime);
+        }
+
+        // prevents movement out of camera
+        if (transform.position.x < -screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+        }
+        if (transform.position.x > screenHalfWidthInWorldUnits)
+        {
+            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+        }
+        if (transform.position.y < halfPlayerHeight)
+        {
+            transform.position = new Vector2(transform.position.x, halfPlayerHeight);
+        }
+        if (transform.position.y > screenHalfHeightInWorldUnits)
+        {
+            transform.position = new Vector2(transform.position.x, screenHalfHeightInWorldUnits);
+        }
+    }
+}
